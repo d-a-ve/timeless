bde65f8f0526f983d0f3df738b8a0c188c6d9ece
Dave <davearonmwan@gmail.com>
2025-01-23 00:44:50 +0100

    feat: add cart drawer

--- src/components/cart/cart-button/cart-button.client.tsx
+++ src/components/cart/cart-button/cart-button.client.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerFooter } from "~/components/ui/drawer";
 import { CartState } from "~/lib/store/cart-store";
 import { useCartStore } from "~/providers/cart-provider";
+import Image from 'next/image'
+
 
 export function CartButtonClient({
   cartProp,
@@ -17,9 +20,39 @@ export function CartButtonClient({
   }, [cartProp, initCart]);
 
   return (
-    <button className="relative flex items-center gap-2 rounded-lg bg-slate-300 p-1">
-      My Cart
-      <span>({cart.length})</span>
-    </button>
+    <Drawer direction="right">
+      <DrawerTrigger asChild>
+        <button className="relative flex items-center gap-2 rounded-lg bg-slate-300 p-1">
+          My Cart
+          <span>({cart.length})</span>
+        </button>
+      </DrawerTrigger>
+      <DrawerContent className="w-[200px] h-96 overscroll-auto">
+        <DrawerHeader>
+          <DrawerTitle>Cart</DrawerTitle>
+        </DrawerHeader>
+        <div>
+          {cart.map(({ product, productCount }) => (
+            <article key={product.id} className="flex items-start gap-3">
+              <div className="size-20 shrink-0">
+                <Image
+                  src={product.thumbnail}
+                  alt={product.title}
+                  width={400}
+                  height={400}
+                  className="size-full"
+                />
+              </div>
+              <div className="grow">
+                <p className="line-clamp-1">{product.title}</p>
+                <p className="line-clamp-1">{product.description}</p>
+              </div>
+              <p className="shrink-0">Count: {productCount}</p>
+            </article>
+          ))}
+        </div>
+        <DrawerFooter></DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }

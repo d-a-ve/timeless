b4b83177b5ae3105cebbc2c7cafd46a45c5607ba
Dave <davearonmwan@gmail.com>
2025-01-27 01:47:23 +0100

    feat: add paystack secrets

--- src/config/config.server.ts
+++ src/config/config.server.ts
@@ -1,5 +1,6 @@
 import { assertEnv } from "./config.utils";
 
 export const CONFIG_SERVER = {
-	APPWRITE_API_KEY: assertEnv("NEXT_APPWRITE_KEY"),
+  APPWRITE_API_KEY: assertEnv("NEXT_APPWRITE_KEY"),
+  PAYSTACK_SECRET_KEY: assertEnv("NEXT_PAYSTACK_SECRET_KEY"),
 };
